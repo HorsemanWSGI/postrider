46f4bf4372faa0083ea5672b7a2f00862d2fae1a
Souheil Chelfouh <trollfot@gmail.com>
2026-02-23 18:52:04 +0100

    migrating to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
 from setuptools import setup
 
-
-setup(
-    name='postrider',
-    install_requires=[
-        'minicli',
-        'dynaconf',
-    ],
-    extras_require={
-        'test': [
-            'pytest',
-        ]
-    }
-)
+setup()
